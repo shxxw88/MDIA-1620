777915d7f6adb4ffdbea2d74384cee1352f44763
Sharleen Wang <sharleenwang7@gmail.com>
2024-09-19 15:42:15 -0700

    initialize stables and add a horse

---
+++ stables.js
@@ -0,0 +1,13 @@
+console.log("horses are great!")
+
+let horseName="Charlie";
+horseName = "Charlie";
+let horseAge = 5;
+console.log(horseName);
+console.log(horseAge);
+let isHorseInStable =false;
+console.log(isHorseInStable);
+isHorseInStable =true;
+horseName ="Coolhorse";
+console.log(horseName);
+console.log(isHorseInStable);

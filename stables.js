66f5db6cae86e4306b3e19121599c9531b7131c3
Sharleen Wang <sharleenwang7@gmail.com>
2024-09-26 15:51:56 -0700

    added horse introduction and discount price

--- stables.js
+++ stables.js
@@ -1,25 +1,32 @@
 console.log("horses are great!")
 
 let horseName="Charlie";
-horseName = "Charlie";
+horseName = "Charles";
 let horseAge = 5;
 console.log(horseName);
 console.log(horseAge);
 let isHorseInStable =false;
 console.log(isHorseInStable);
 isHorseInStable =true;
-horseName ="Coolhorse";
-console.log(horseName);
 console.log(isHorseInStable);
 
 const STABLE_MONTHTLY_FEE=100;
 console.log(STABLE_MONTHTLY_FEE);
-console.log(3+2);
-console.log(STABLE_MONTHTLY_FEE**3);
 
-let horseNickName="Charles"
-let horseIntroduction=" is the same of my horse"
-console.log(horseName+ horseIntroduction);
 
-let rentIntroduction=`it costs ${STABLE_MONTHTLY_FEE} to board ${horseName}`;
+// Lesson 4 Code //
+
+let horseNickname="Cheese";
+console.log(horseNickname);
+
+let welcomeMessage="Welcome to my stable!";
+console.log(welcomeMessage);
+
+let horseIntroduction=`This horse's name is ${horseName} and his nickname is ${horseNickname}`;
+console.log(horseIntroduction);
+
+let rentIntroduction=`It normally costs ${STABLE_MONTHTLY_FEE} per month to board with ${horseNickname}`;
 console.log(rentIntroduction);
+
+let rentDiscount=`For a 3 months stay, you can get a 10% discount for ${(STABLE_MONTHTLY_FEE*3)*0.9} dollars!`;
+console.log(rentDiscount);

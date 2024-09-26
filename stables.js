87f1d92ff3ca6ac3941695745ca909cbf2b12149
Sharleen Wang <sharleenwang7@gmail.com>
2024-09-26 14:37:35 -0700

    add operators to my stables

--- stables.js
+++ stables.js
@@ -11,3 +11,14 @@ isHorseInStable =true;
 horseName ="Coolhorse";
 console.log(horseName);
 console.log(isHorseInStable);
+
+const STABLE_MONTHTLY_FEE=100;
+console.log(STABLE_MONTHTLY_FEE);
+console.log(3+2);
+console.log(STABLE_MONTHTLY_FEE**3);
+
+let horseIntroduction=" is the same of my horse"
+console.log(horseName+ horseIntroduction);
+
+let rentIntroduction=`it costs ${STABLE_MONTHTLY_FEE} to board ${horseName}`;
+console.log(rentIntroduction);
